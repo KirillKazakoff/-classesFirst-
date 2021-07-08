ff0f53264f191333c7ac48702d9eac772fe51570
keka3avr <kekazavr228@gmail.com>
2021-07-08 18:24:25 +1100

    index.js adjusted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './css/style.css';
+import Daemon from './js/subClasses/daemon';
+import Bowman from './js/subClasses/bowman';
+import Magician from './js/subClasses/magician';
+import Swordman from './js/subClasses/swordman';
+import Undead from './js/subClasses/undead';
+import Zombie from './js/subClasses/zombie';
