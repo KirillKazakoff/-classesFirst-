2e8c8dd51d82be9b33bc75b082952208cd9cf771
keka3avr <kekazavr228@gmail.com>
2021-07-08 17:47:31 +1100

    tests fixed

--- src/__tests__/character.test.js
+++ src/__tests__/character.test.js
@@ -1,16 +1,16 @@
 import Bowman from '../js/subClasses/bowman';
 
 test('too long name inputed', () => {
-    const check = () => new Bowman('toobignameforit', 'Bowman');
+    const check = () => new Bowman('toobignameforit');
     expect(check).toThrow('too long name inputed');
 });
 
 test('too short name inputed', () => {
-    const check = () => new Bowman('t', 'Bowman');
+    const check = () => new Bowman('t');
     expect(check).toThrow('too short name inputed');
 });
 
 test('incorrect value input', () => {
-    const check = () => new Bowman(1, 'Bowman');
+    const check = () => new Bowman(1);
     expect(check).toThrow('incorrect value input');
 });
